20a83e4fa94aa8a4742504028b78e2d6546a07a2
OWG\bryce.chamberlain <bryce.chamberlain@oliverwyman.com>
2022-02-17 11:19:01 -0600

    Change file name.

--- fun/correlatedfeatures-find.R
+++ fun/correlatedfeatures-find.R
@@ -1,5 +1,5 @@
 # https://medium.com/@outside2SDs/an-overview-of-correlation-measures-between-categorical-and-continuous-variables-4c7f85610365
-find_correlated_features = function(x, verbose = TRUE, run_autotype = TRUE){
+correlatedfeatures_find = function(x, verbose = TRUE, run_autotype = TRUE){
 
     x = as.superframe(x, run_autotype = run_autotype)
     x$correlated_features = list()
