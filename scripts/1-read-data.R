37d817e8c2a03ff096dd23eb5e37e9e2736c9ec9
bryce <superchordate@gmail.com>
2022-02-23 20:36:06 -0600

    Change to claims dataset

--- scripts/1-read-data.R
+++ scripts/1-read-data.R
@@ -1,37 +1,14 @@
-if(!cache.ok(1)){
+if(!cache.ok(1)){    
+  
+    # https://www.kaggle.com/mykeysid10/insurance-claims-fraud-detection
+    dt = read.any('in/fraud_insurance_claims.csv')
     
-    # https://www.kaggle.com/mur418/2020-fantasy-football/data?select=rb_stats_and_projections.csv    
-    dofiles = list.files('in', pattern = 'projections.csv$', full.names = TRUE)
-    dofiles = dofiles[!grepl('wr_stats', dofiles)] # this file uses different column names. drop for now.
+    dt %<>%
+      mutate_at(
+        vars(policy_bind_date, incident_date), 
+        list(parse_date_time), 
+        orders = 'ymd_HMS'
+      )
     
-    dt = bind_rows(lapply(
-        dofiles,
-        function(file) read.any(
-            file, 
-            folder = 'in', 
-            all_chars = TRUE,
-            first_column_name = 'TEAM NAME',
-            field_name_map = c(
-            'Player OUTLOOK' = '2020 OUTLOOK',
-            'PLAYER OUTLOOK' = '2020 OUTLOOK'
-            )
-        ) %>% 
-        select(-`NA`)
-    )) %>%
-        atype() %>%
-        filter(`2020 FPTS` > 0)
-
-    # manual data fixes for this specific dataset.
-
-        # replace nas with 0s in numeric columns.
-        for(col in names(dt)) if(is.numeric(dt[[col]])) dt[[col]][ is.na(dt[[col]]) ] <- 0
-
-        # NA teams = no team.
-        dt$`TEAM NAME`[ is.na(dt$`TEAM NAME`) ] <- 'noteam'
-        
-        # No outlook available = NA
-        dt[dt == "No outlook available."] <- NA
-
     save.cache(dt)
-    
 }
